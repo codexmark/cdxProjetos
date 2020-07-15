167ab0300d971fb83fa2a5acc7098e8a7041e4fe
Mark Mesquita <codexmark@gmail.com>
2020-07-14 20:16:28 -0400

    correção de importação sem uso

--- cdxDeliveryApp/lib/views/layout.dart
+++ cdxDeliveryApp/lib/views/layout.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Layout {
   static Widget render(Widget content, [String title = 'Delivery App']) {
